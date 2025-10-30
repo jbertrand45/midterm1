9385d80fddc24a7f22a033e4e3fd044fa28d5078
Joey Bertrand <jbertrand@nevada.unr.edu>
2025-10-30 02:48:36 -0700

    displaying user profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState, useEffect } from "react"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
+import "./App.css"
+import { Amplify } from "aws-amplify"
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore: implicitly has an 'any' type because aws-exports.js lacks a declaration file
+import outputs from "./aws-exports.js"
+import { generateClient } from "aws-amplify/api"
+import "@aws-amplify/ui-react/styles.css"
+import { useAuthenticator } from "@aws-amplify/ui-react"
+
+Amplify.configure(outputs)
+
+// Generate API client
+const client = generateClient({
+  authMode: "userPool",
+})
 
 function App() {
+  const { user, signOut } = useAuthenticator((context) => [context.user])
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+
+  }, [])
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+
+      <h1>Vite + React + Amplify</h1>
+      <p>Signed in as: {user?.username}</p>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           Favorite Restaurant : Teriyaki Madness count : {count}
@@ -25,6 +48,11 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+
+      <button onClick={signOut} style={{ marginTop: "1rem" }}>
+        Sign out
+      </button>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
@@ -33,3 +61,5 @@ function App() {
 }
 
 export default App
+
+
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {Authenticator} from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Authenticator>
+      <App />
+    </Authenticator>
   </StrictMode>,
 )
